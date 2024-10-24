a1ca564c66133dae831822e120dacc0078b7391a
theshpio <student-shpio@proton.me>
2024-10-25 01:19:05 +0200

    feat(ci): Add coveralls, badge, cache

--- scripts/run_coverage.sh
+++ scripts/run_coverage.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# exist the script if any command fails
+set -e
+
 # Check if SKIP_CRATE is set
 if [ -z "$SKIP_CRATE" ]; then
   echo "No crate to skip. Running tarpaulin for all crates."
@@ -24,4 +27,17 @@ for crate in $(find packages -name Cargo.toml -exec dirname {} \;); do
   cargo tarpaulin --manifest-path $crate/Cargo.toml --out lcov \
     --output-dir $crate/coverage --exclude-files "/target/**/*" "/tarpaulin_temp/**/*" \
     --target-dir $crate/tarpaulin_temp --skip-clean || echo "Tarpaulin failed for $CRATE_NAME"
+
+  # Check if the coverage report exists and upload it
+  if [ -f "$crate/coverage/lcov.info" ]; then
+    echo "Uploading coverage for $CRATE_NAME"
+    coveralls --lcov-file "$crate/coverage/lcov.info" --repo-token $COVERALLS_REPO_TOKEN
+  else
+    echo "No lcov.info file found for $CRATE_NAME"
+  fi
+
+  # Generate a badge link and store it in a markdown file for the crate
+  echo "Generating badge for $CRATE_NAME"
+  echo "[![Coverage Status](https://coveralls.io/repos/github/Meta-A/bbpm/${CRATE_NAME}/badge.svg?branch=main)](https://coveralls.io/github/Meta-A/bbpm/${CRATE_NAME}?branch=main)" > "${CRATE_NAME}.md"
+  echo "Badge for $CRATE_NAME saved in ${CRATE_NAME}.md"
 done
